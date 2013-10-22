dc2d162fcc37952bc77dd93cac68f83359523bff
susanngu <susanngu@stud.ntnu.no>
2013-10-22 11:22:50 -0700

    ny

---
+++ MovieShare/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieShare::Application.config.secret_token = '3e35e0405e35cfedc02c3c452d0d9fda47c1e1d28fe93e6ddcc4fbb4ce21c7f54d88c901deacc5902640ca1af86f34ee808bbc476be0f9b8c113fbad541be87a'
