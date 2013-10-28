4c846c445d37f7e54cddb88bbb5def51ecf83bc6
Simon Hordvik <simon.hordvik@gmail.com>
2013-10-28 15:47:57 -0700

    demo stuff

--- MovieShare/app/controllers/users_controller.rb
+++ MovieShare/app/controllers/users_controller.rb
@@ -5,15 +5,23 @@ class UsersController < ActionController::Base
 
   def create
     @user = User.new(params[:user])
-    if @user.save
-      flash[:notice] = "You signed up successfully"
-      flash[:color] = "valid"
-    else
-      flash[:notice] = "Form is invalid"
-      flash[:notice] = "invalid"
-    end
 
-    render "new"
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to '/pages/mainpage', notice: "User #{@user.name} was successfully created." }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def index
+    @users = User.order(:name)
+    respond_to do |format|
+      format.html # index.html.erb format.json { render json: @users }
+    end
   end
 
 end
