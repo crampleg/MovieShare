c1669f5b4d8f92eeadb684b128396e8090649873
Simon Hordvik <simon.hordvik@gmail.com>
2013-10-28 16:33:25 -0700

    asdasd

--- MovieShare/app/controllers/users_controller.rb
+++ MovieShare/app/controllers/users_controller.rb
@@ -17,11 +17,5 @@ class UsersController < ActionController::Base
     end
   end
 
-  def index
-    @users = User.order(:name)
-    respond_to do |format|
-      format.html # index.html.erb format.json { render json: @users }
-    end
-  end
 
 end
