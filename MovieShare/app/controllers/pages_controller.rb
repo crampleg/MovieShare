ce70aa24e0e7649ad4911b3c37bc3a72ec011050
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2013-10-30 13:04:41 -0700

    movie search

--- MovieShare/app/controllers/pages_controller.rb
+++ MovieShare/app/controllers/pages_controller.rb
@@ -6,42 +6,17 @@ class PagesController < ApplicationController
 
     query = params[:query]
     query.gsub! /\s+/, "+"
-    url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=6ayr4sd4j3jrhr5srzu5rc8n&q=#{query}&page_limit=10"
+    url = "http://mymovieapi.com/?title=#{query}&type=json&plot=simple&episode=0&limit=10&yg=0&mt=M&lang=en-US&offset=&aka=simple&release=simple&business=0&tech=0"
+    
     begin
       response = Net::HTTP.get(URI.parse(url))
       parsed_json = ActiveSupport::JSON.decode(response)
-      logger.info parsed_json
+      $movies = parsed_json
     rescue SocketError => e
       puts e.message
     end
-
-    $listsize = 0
-    $total = parsed_json["total"]
-    if $total >= 10
-      $listsize = 10
-    else 
-      $listsize = $total
-    end
-
-    $j = 0
-    $movies = {}
-    for i in (1..$listsize)
-      $movies[i] = {
-        poster: $parsed_json["movies"][$j]["posters"]["original"]
-      }
-      $j += 1
-    end
-
-    $id = parsed_json["movies"][0]["id"]
-    $title = parsed_json["movies"][0]["title"]
-    $year = parsed_json["movies"][0]["year"]
-    $rating = parsed_json["movies"][0]["mpaa_rating"]
-    $runtime = parsed_json["movies"][0]["runtime"]
-    $theater = parsed_json["movies"][0]["release_dates"]["theater"]
-    $dvd = parsed_json["movies"][0]["release_dates"]["dvd"]
-    $synopsis = parsed_json["movies"][0]["synopsis"]
-    $poster = parsed_json["movies"][0]["posters"]["original"]
-
+    
     redirect_to '/pages/list'
+  
   end
 end
--- MovieShare/db/schema.rb
+++ MovieShare/db/schema.rb
@@ -20,21 +20,14 @@ ActiveRecord::Schema.define(:version => 20131030005111) do
     t.datetime "updated_at",       :null => false
   end
 
-  create_table "logins", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "firstname"
     t.string   "lastname"
     t.string   "email"
-    t.string   "gender"
-    t.string   "encrypted_password"
-    t.string   "salt"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
