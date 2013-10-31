8ea16078f09b52369b7fc94588072cc9ffa308cc
Simon Hordvik <simon.hordvik@gmail.com>
2013-10-31 15:24:38 -0700

    asd

--- MovieShare/app/controllers/login_controller.rb
+++ MovieShare/app/controllers/login_controller.rb
@@ -1,4 +1,4 @@
-class LoginController < ActiveRecord::Base
+class LoginController < ActionController::Base
   #skip_before_filter :authorize   #everyone can access the methods in this controller by logging in (whitelisting)
   def new
   end
@@ -7,10 +7,9 @@ class LoginController < ActiveRecord::Base
     user = User.find_by_username(params[:username])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id           #session is a built-in feature of rails that keep track of users logged in via userID
-      user.following = 0
-      user.followers = 0
+      user.following = '0'
+      user.followers = '0'
       user.save!
-      user.reload
       $current_user = user  #sets the current user to the global value $user
       redirect_to '/pages/mainpage', notice: "Welcome #{user.firstname}!"
     else
