45a6c65dbf162e2cbc99f7426a575df2b2b8fa6f
Simon Hordvik <simon.hordvik@gmail.com>
2013-10-31 13:07:24 -0700

    kj

--- MovieShare/app/controllers/application_controller.rb
+++ MovieShare/app/controllers/application_controller.rb
@@ -4,10 +4,7 @@ class ApplicationController < ActionController::Base
 
   protected
   def authorize
-    unless User.find_by_id(session[:user_id])                #the session list must contain a userID
-      redirect_to '/login/new', notice: "Please log in"
-    end
-    if (!$current_user)                                     #the $current_user variable cant be nil
+    unless User.find_by_id(session[:user_id]) || $current_user                #the session list must contain a userID
       redirect_to '/login/new', notice: "Please log in"
     end
   end
