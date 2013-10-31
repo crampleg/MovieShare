53c99a3e9164c9b8b8e6e18f7645c13944b78f1c
Simon Hordvik <simon.hordvik@gmail.com>
2013-10-31 14:53:05 -0700

    db

--- MovieShare/app/controllers/login_controller.rb
+++ MovieShare/app/controllers/login_controller.rb
@@ -7,7 +7,10 @@ class LoginController < ActionController::Base
     user = User.find_by_username(params[:username])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id           #session is a built-in feature of rails that keep track of users logged in via userID
-      $current_user = user;  #sets the current user to the global value $user
+      $current_user = user  #sets the current user to the global value $user
+      user.following = 0
+      user.followers = 0
+      user.save
       redirect_to '/pages/mainpage', notice: "Welcome #{user.firstname}!"
     else
       redirect_to '/login/new', alert: "Invalid username/password combination"
