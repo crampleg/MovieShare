7f57f18a18c0f7f979e2c635a540671eeb5eb94f
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-04 12:16:28 -0800

    asdasd

--- MovieShare/db/schema.rb
+++ MovieShare/db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(:version => 20131031212111) do
     t.integer  "owner_id"
     t.string   "movie_name"
     t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "updated_at", :null => false 
   end
 
   create_table "users", :force => true do |t|
