9d9bb973e2b3febf25853fa5d5e9096e49b9c699
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-09 20:44:11 -0800

    asd

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,6 +1,7 @@
 class Follower < ActiveRecord::Base
   attr_accessible :user_id_follower, :user_id_model
-
+  validates :user_id_follower, :uniqueness => { :scope => :user_id_model }  #makes sure that a user cant follow a specific user multiple times
+  
   belongs_to :user
   #Test the following to see what the output is:
   #fol = Follower.find(...)
