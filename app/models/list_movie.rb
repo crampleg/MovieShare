2ae513a5b4b2706847d4e217000083e98c9b79cb
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-10 16:39:02 -0800

    asd

--- app/models/list_movie.rb
+++ app/models/list_movie.rb
@@ -1,3 +1,4 @@
 class ListMovie < ActiveRecord::Base
   attr_accessible :list_id, :movie_name
+  validates :list_id, :uniqueness => { :scope => :movie_name }  #makes sure that a movie can't be added to a list more than once
 end
--- app/models/my_list.rb
+++ app/models/my_list.rb
@@ -1,3 +1,5 @@
 class MyList < ActiveRecord::Base
   attr_accessible :list_name, :owner_id
+  
+  validates :owner_id, :uniqueness => { :scope => :list_name }  #makes sure that a user can't create multiple lists with same name
 end
