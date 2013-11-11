6cb11089c418fe876f5746e390365fd7ec6e2be4
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-10 21:17:40 -0800

    asd

--- app/models/my_list.rb
+++ app/models/my_list.rb
@@ -1,5 +1,6 @@
 class MyList < ActiveRecord::Base
   attr_accessible :list_name, :owner_id
   
+  validates :list_name, :presence => true
   validates :owner_id, :uniqueness => { :scope => :list_name }  #makes sure that a user can't create multiple lists with same name
 end
