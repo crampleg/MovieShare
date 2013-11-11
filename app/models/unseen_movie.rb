80690a6ec428f96cce81b41703f9b77e5c4db19e
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-10 16:35:21 -0800

    asd

--- app/models/unseen_movie.rb
+++ app/models/unseen_movie.rb
@@ -1,3 +1,4 @@
 class UnseenMovie < ActiveRecord::Base
   attr_accessible :movie_name, :owner_id
+  validates :owner_id, :uniqueness => { :scope => :movie_name }  #makes sure that same movie can't be added more than once
 end
