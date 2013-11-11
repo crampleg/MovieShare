4678f6059ef557a1a196a41417b786d6fc09c251
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-10 20:41:27 -0800

    asd

--- db/migrate/20131031212047_add_following_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddFollowingToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :following_no, :integer
-  end
-end
--- db/migrate/20131031212111_add_followers_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddFollowersToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :followers_no, :integer
-  end
-end
