ae106811bbfcae65d760bcbe7508081149dde750
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-12 20:31:23 -0800

    asd

--- script/load_users.rb
+++ script/load_users.rb
@@ -1,3 +1,4 @@
 (1..10000).each do |i|
-  User.create(username: "Customer #{i}", firstname: "John#{i}", lastname: "Doe#{i}", email: "john.doe#{i}@gmail.com", password_digest: "Password#{i}", description: "MyDescription#{i}") 
+  user = User.create(username: "johnny#{i}", firstname: "John#{i}", lastname: "Doe#{i}", email: "john.doe#{i}@gmail.com", password_digest: "Password#{i}", description: "MyDescription#{i}") 
+  WatchedMovie.create(user_id: user.id, movies: "")
 end
