5c127d82d9d1c70067da40986f6744dcb7972cb2
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-12 19:56:19 -0800

    asdasd

--- script/load_users.rb
+++ script/load_users.rb
@@ -1,4 +1,3 @@
-User.transaction do (1..10000).each do |i|
-  User.create(username: "Customer #{i}", firstname: "John#{i}", lastname: "Doe#{i}", email: "john.doe#{i}@gmail.com", password_digest: "Password#{i}", description: "MyDescription#{i}")
-  end 
+(1..10000).each do |i|
+  User.create(username: "Customer #{i}", firstname: "John#{i}", lastname: "Doe#{i}", email: "john.doe#{i}@gmail.com", password_digest: "Password#{i}", description: "MyDescription#{i}") 
 end
