5adde3af67b75f188f30d168871f164d5e5ae1b3
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-18 15:09:16 -0800

    load movies

---
+++ script/load_watched_movies.rb
@@ -0,0 +1,5 @@
+(1..100).each do |i|
+  user_watched = WatchedMovie.find_by_user_id(i)
+  user_watched.movies = "tt0462538;tt0234215;tt0111161;tt0068646;tt0110912;tt0060196;tt0468569;tt0050083;tt0108052;tt0167260;tt0137523;tt0080684"
+  user_watched.save
+end
