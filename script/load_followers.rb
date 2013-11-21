7f7608ad0eb648f66c7189890e814b40dc1ba9b4
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-21 14:34:20 -0800

    load followers

--- script/load_followers.rb
+++ script/load_followers.rb
@@ -1,5 +1,24 @@
-(1..100).each do |i|
+CONN = ActiveRecord::Base.connection
+
+inserts1 = []
+password = BCrypt::Password.create("movieshare");
+(1..99900).each do |i|
   (i+1..(i+1)+100).each do|j|
-    Follower.create(user_id_model: i, user_id_follower: j)
+    inserts1.push "(null, #{i}, #{j})"
   end
 end
+
+inserts2 = []
+password = BCrypt::Password.create("movieshare");
+(99901..100001).each do |i|
+  (1..100).each do|j|
+    inserts2.push "(null, #{i}, #{j})"
+  end
+end
+
+
+sql1 = "INSERT INTO followers VALUES #{inserts1.join(", ")}"
+sql2 = "INSERT INTO followers VALUES #{inserts2.join(", ")}"
+
+CONN.execute sql1
+CONN.execute sql2
--- script/load_users.rb
+++
@@ -1,4 +0,0 @@
-(1..10000).each do |i|
-  user = User.create(username: "johnny#{i}", firstname: "John#{i}", lastname: "Doe#{i}", email: "john.doe#{i}@gmail.com", password_digest: BCrypt::Password.create("movieshare"), description: "MyDescription#{i}") 
-  WatchedMovie.create(user_id: user.id, movies: "")
-end
