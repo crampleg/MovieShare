e10c4ece23bb7839c068317dfc5203b4f9b72930
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-21 13:24:09 -0800

    asd

--- script/load_100_000_users.rb
+++ script/load_100_000_users.rb
@@ -1,12 +1,15 @@
 CONN = ActiveRecord::Base.connection
 
-inserts = []
+inserts1 = []
+insert2 = []
 password = BCrypt::Password.create("movieshare");
 (1..10).each do |i|
   #user = User.create(username: "johnny#{i}", firstname: "John#{i}", lastname: "Doe#{i}", email: "john.doe#{i}@gmail.com", password_digest: BCrypt::Password.create("movieshare"), description: "MyDescription#{i}") 
   #WatchedMovie.create(user_id: user.id, movies: "")
-  inserts.push "(null, 'johnny#{i}', 'John', 'Doe#{i}', 'john.doe#{i}@gmail.com', '#{password}', '2013-11-14 22:48:22', '2013-11-14 22:48:22', 'Description about Johnny#{i}')"
+  inserts1.push "(null, 'johnny#{i}', 'John', 'Doe#{i}', 'john.doe#{i}@gmail.com', '#{password}', '2013-11-14 22:48:22', '2013-11-14 22:48:22', 'Description about Johnny#{i}')"
+  insert2.push "(null, #{i}, '', '2013-11-14 22:48:22', '2013-11-14 22:48:22')"
 end
-sql = "INSERT INTO users VALUES #{inserts.join(", ")}"
+sql1 = "INSERT INTO users VALUES #{inserts1.join(", ")}"
+sql2 = "INSERT INTO watched_movies VALUES #{inserts2.join(", ")}"
 
 CONN.execute sql
