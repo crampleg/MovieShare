7ccace20f1d691209d66e432bfbd4ae04348b47f
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-21 15:13:11 -0800

    asd

--- script/load_followers.rb
+++ script/load_followers.rb
@@ -2,7 +2,7 @@ CONN = ActiveRecord::Base.connection
 
 inserts1 = []
 password = BCrypt::Password.create("movieshare");
-(1..99990).each do |i|
+(1..9990).each do |i|
   (i+1..(i+1)+9).each do|j|
     inserts1.push "(null, #{i}, #{j}, '2013-11-14 22:48:22', '2013-11-14 22:48:22')"
   end
@@ -18,7 +18,7 @@ end
 
 
 sql1 = "INSERT INTO followers VALUES #{inserts1.join(", ")}"
-sql2 = "INSERT INTO followers VALUES #{inserts2.join(", ")}"
+#sql2 = "INSERT INTO followers VALUES #{inserts2.join(", ")}"
 
 CONN.execute sql1
-CONN.execute sql2
+#CONN.execute sql2
