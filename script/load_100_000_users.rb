fa242769c9411452a1433d231bdb4acf62cf42ec
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-21 14:20:14 -0800

    asdasdasd

--- script/load_100_000_users.rb
+++ script/load_100_000_users.rb
@@ -4,8 +4,6 @@ inserts1 = []
 inserts2 = []
 password = BCrypt::Password.create("movieshare");
 (1..100000).each do |i|
-  #user = User.create(username: "johnny#{i}", firstname: "John#{i}", lastname: "Doe#{i}", email: "john.doe#{i}@gmail.com", password_digest: BCrypt::Password.create("movieshare"), description: "MyDescription#{i}") 
-  #WatchedMovie.create(user_id: user.id, movies: "")
   inserts1.push "(null, 'johnny#{i}', 'John', 'Doe#{i}', 'john.doe#{i}@gmail.com', '#{password}', '2013-11-14 22:48:22', '2013-11-14 22:48:22', 'Description about Johnny#{i}')"
   inserts2.push "(null, #{i}, '', '2013-11-14 22:48:22', '2013-11-14 22:48:22')"
 end
