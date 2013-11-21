f43b81b6f0e0a9411de6e5158f80797c921517e7
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-21 13:06:06 -0800

    asd

--- script/load_100_000_users.rb
+++ script/load_100_000_users.rb
@@ -2,10 +2,10 @@ CONN = ActiveRecord::Base.connection
 
 inserts = []
 password = BCrypt::Password.create("movieshare");
-(1..100000).each do |i|
+(1..10).each do |i|
   #user = User.create(username: "johnny#{i}", firstname: "John#{i}", lastname: "Doe#{i}", email: "john.doe#{i}@gmail.com", password_digest: BCrypt::Password.create("movieshare"), description: "MyDescription#{i}") 
   #WatchedMovie.create(user_id: user.id, movies: "")
-  inserts.push "(#{i}, 'johnny#{i}', 'John', 'Doe#{i}', 'john.doe#{i}@gmail.com', '#{password}', '2013-11-14 22:48:22', '2013-11-14 22:48:22', 'Description about Johnny#{i}')"
+  inserts.push "(null, 'johnny#{i}', 'John', 'Doe#{i}', 'john.doe#{i}@gmail.com', '#{password}', '2013-11-14 22:48:22', '2013-11-14 22:48:22', 'Description about Johnny#{i}')"
 end
 sql = "INSERT INTO users VALUES #{inserts.join(", ")}"
 
