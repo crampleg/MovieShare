4e2b50a646fc4497f957c8278a78dc5bf22afae3
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-26 19:46:46 -0800

    ads

--- script/load_followers.rb
+++ script/load_followers.rb
@@ -3,18 +3,18 @@ CONN = ActiveRecord::Base.connection
 inserts1 = []
 password = BCrypt::Password.create("movieshare");
 (1..99990).each do |i|
-  (i+1..(i+1)+9).each do|j|
+  (i+1..(i+1)+2).each do|j|
     inserts1.push "(null, #{i}, #{j}, '2013-11-14 22:48:22', '2013-11-14 22:48:22')"
   end
 end
 
-inserts2 = []
-password = BCrypt::Password.create("movieshare");
-(99991..100000).each do |i|
-  (1..10).each do|j|
-    inserts2.push "(null, #{i}, #{j}, '2013-11-14 22:48:22', '2013-11-14 22:48:22')"
-  end
-end
+#inserts2 = []
+#password = BCrypt::Password.create("movieshare");
+#(99991..100000).each do |i|
+#  (1..10).each do|j|
+#    inserts2.push "(null, #{i}, #{j}, '2013-11-14 22:48:22', '2013-11-14 22:48:22')"
+#  end
+#end
 
 
 sql1 = "INSERT INTO followers VALUES #{inserts1.join(", ")}"
--- script/load_watched_movies.rb
+++ script/load_watched_movies.rb
@@ -1,10 +1,11 @@
+CONN = ActiveRecord::Base.connection
 inserts = []
 (1..100).each do |i|
   #user_watched = WatchedMovie.find_by_user_id(i)
   #user_watched.movies = "tt0462538;tt0234215;tt0111161;tt0068646;tt0110912;tt0060196;tt0468569;tt0050083;tt0108052;tt0167260;tt0137523;tt0080684"
   #user_watched.save
-  inserts.push "(null, #{i}, '', '2013-11-14 22:48:22', '2013-11-14 22:48:22')"
+  inserts.push "('tt0462538;tt0234215;tt0111161;tt0068646;tt0110912;tt0060196;tt0468569;tt0050083;tt0108052;tt0167260;tt0137523;tt0080684')"
 end
-sql = "INSERT INTO users VALUES #{inserts.join(", ")}"
+sql = "UPDATE watched_movies SET movie_name = #{inserts.join(", ")}"
 
 CONN.execute sql
