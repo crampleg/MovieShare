33e365dd86c13d96b77d05ed190635493704756c
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-26 19:47:27 -0800

    asd

--- script/load_watched_movies.rb
+++ script/load_watched_movies.rb
@@ -6,6 +6,6 @@ inserts = []
   #user_watched.save
   inserts.push "('tt0462538;tt0234215;tt0111161;tt0068646;tt0110912;tt0060196;tt0468569;tt0050083;tt0108052;tt0167260;tt0137523;tt0080684')"
 end
-sql = "UPDATE watched_movies SET movie_name = #{inserts.join(", ")}"
+sql = "UPDATE watched_movies SET movies = #{inserts.join(", ")}"
 
 CONN.execute sql
