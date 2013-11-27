60603e612139f085f0f7fb52ac4db3d27ab9668b
Simon Hordvik <simon.hordvik@gmail.com>
2013-11-26 19:58:55 -0800

    asd

--- script/load_watched_movies.rb
+++ script/load_watched_movies.rb
@@ -1,11 +1,13 @@
-CONN = ActiveRecord::Base.connection
-inserts = []
-(1..100).each do |i|
-  #user_watched = WatchedMovie.find_by_user_id(i)
-  #user_watched.movies = "tt0462538;tt0234215;tt0111161;tt0068646;tt0110912;tt0060196;tt0468569;tt0050083;tt0108052;tt0167260;tt0137523;tt0080684"
-  #user_watched.save
-  inserts.push "('tt0462538;tt0234215;tt0111161;tt0068646;tt0110912;tt0060196;tt0468569;tt0050083;tt0108052;tt0167260;tt0137523;tt0080684')"
+#CONN = ActiveRecord::Base.connection
+#inserts = []
+ActiveRecord::Base.transaction do
+  (1..100000).each do |i|
+    user_watched = WatchedMovie.find_by_user_id(i)
+    user_watched.movies = "tt0462538;tt0234215;tt0111161;tt0068646;tt0110912;tt0060196;tt0468569;tt0050083;tt0108052;tt0167260;tt0137523;tt0080684"
+    user_watched.save
+    #inserts.push     #"('tt0462538;tt0234215;tt0111161;tt0068646;tt0110912;tt0060196;tt0468569;tt0050083;tt0108052;tt0167260;tt0137523;tt00806#84')"
+  end
 end
-sql = "UPDATE watched_movies SET movies = #{inserts.join(", ")}"
+#sql = "UPDATE watched_movies SET movies = #{inserts.join(", ")}"
 
-CONN.execute sql
+#CONN.execute sql
