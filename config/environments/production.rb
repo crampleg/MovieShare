af8fb943a65f75d36a1cc046f619cdcbb841a657
Simon Hordvik <simon.hordvik@gmail.com>
2013-12-06 10:50:29 -0800

    asd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ MovieShare::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  config.cache_store = :dalli_store { :expires_in => 1.minutes }
+  config.cache_store = :dalli_store, { :expires_in => 1.minutes }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
